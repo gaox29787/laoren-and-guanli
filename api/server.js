d89d4ec2893997016168b10f82487cbebfaeadc2
gaox29787 <gaox29787@gmail.com>
2026-04-29 15:20:08 +0800

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const jsonServer = require('json-server');
+const path = require('path');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router(path.join(__dirname, '../db.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
@@ -20,5 +21,5 @@ server.use('/api', router);
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`JSON Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
